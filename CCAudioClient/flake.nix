ad19e3f9bfb4670fdf86d2fc098790d4e662b036
Memphis Herghelegiu <memphispc70@gmail.com>
2026-07-14 07:40:10 +0200

    updated some stuff

---
+++ CCAudioClient/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "CCAudioClient development shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+    outputs = { self, nixpkgs }:
+        let
+          system = "x86_64-linux";
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells.${system}.default = pkgs.mkShell {
+            packages = [
+              pkgs.craftos-pc
+            ];
+
+            shellHook = ''
+              echo "========================================================="
+              echo " CC:Tweaked Dev Shell Active "
+              echo " Write your Lua scripts inside the './src' directory."
+              echo " Run 'craftos' to start the emulator."
+              echo " Run 'mount src ./src' inside CraftOS to link files."
+              echo "========================================================="
+            '';
+          };
+    };
+}
--- CCAudioServer/src/main/kotlin/main.kt
+++ CCAudioServer/src/main/kotlin/main.kt
@@ -3,6 +3,8 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.application.*
 
+data class Test(val a : Int)
+
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
